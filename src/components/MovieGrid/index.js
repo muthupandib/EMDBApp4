4ef8363596a1463fb9e714be50c8bf46d750444e
muthupandib <muthupandib0067@gmail.com>
2024-12-02 08:44:23 +0000

    Enhancemovie

--- src/components/MovieGrid/index.js
+++ src/components/MovieGrid/index.js
@@ -1,22 +1,24 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 import './index.css'
 
-const MovieGrid = ({movies}) => {
+const MovieCard = props => {
+  const {movieDetails} = props
+  const {id, title, posterPath, voteAverage} = movieDetails
+
   return (
-    <div className='movie-grid'>
-      {movies.map(movie => (
-        <div key={movie.id} className='movie-card'>
-          <img
-            src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
-            alt={movie.title}
-            className='movie-image'
-          />
-          <h3 className='movie-title'>{movie.title}</h3>
-          <p className='movie-rating'>Rating: {movie.vote_average}</p>
-        </div>
-      ))}
-    </div>
+    <li className='movie-card-container col-12 col-sm-6 col-lg-2 mb-3 d-flex flex-column'>
+      <img className='movie-card-image' alt={title} src={posterPath} />
+      <div className='d-flex flex-column align-items-center mt-2'>
+        <h1 className='movie-title m-0'>{title}</h1>
+        <p className='movie-rating mb-0 ms-1'>Rating: {voteAverage}</p>
+      </div>
+      <Link to={`/movie/${id}`} className='mt-auto align-self-center'>
+        <button className='btn btn-outline-success' type='button'>
+          View Details
+        </button>
+      </Link>
+    </li>
   )
 }
 
-export default MovieGrid
+export default MovieCard
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,41 +1,74 @@
-import React, {useState} from 'react'
-import {Link, useHistory} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom'
+
+import SearchMoviesContext from '../../context/SearchMoviesContext'
+
 import './index.css'
 
-const Navbar = () => {
-  const [searchQuery, setSearchQuery] = useState('')
-  const history = useHistory()
+const NavBar = props => {
+  const renderSearchBar = () => (
+    <SearchMoviesContext.Consumer>
+      {value => {
+        const {onTriggerSearchingQuery, onChangeSearchInput, searchInput} =
+          value
 
-  const handleSearchChange = event => {
-    setSearchQuery(event.target.value)
-  }
+        const onChangeHandler = event => onChangeSearchInput(event.target.value)
 
-  const handleSearch = () => {
-    if (searchQuery) {
-      history.push(`/search/${searchQuery}`)
-    }
-  }
+        const onSearchHandler = event => {
+          event.preventDefault()
+          const {history} = props
+          onTriggerSearchingQuery()
+          history.push(`/search`)
+        }
+
+        return (
+          <div className='d-flex align-items-center'>
+            <input
+              type='text'
+              className='me-2 search-input'
+              onChange={onChangeHandler}
+              value={searchInput}
+              placeholder='Search'
+            />
+            <button
+              className='btn btn-outline-info'
+              type='button'
+              onClick={onSearchHandler}
+            >
+              Search
+            </button>
+          </div>
+        )
+      }}
+    </SearchMoviesContext.Consumer>
+  )
 
   return (
-    <nav className='navbar'>
-      <Link to='/' className='navbar-link'>
-        Popular
-      </Link>
-      <Link to='/top-rated' className='navbar-link'>
-        Top Rated
-      </Link>
-      <Link to='/upcoming' className='navbar-link'>
-        Upcoming
-      </Link>
-      <input
-        type='text'
-        placeholder='Search movies...'
-        value={searchQuery}
-        onChange={handleSearchChange}
-      />
-      <button onClick={handleSearch}>Search</button>
+    <nav className='navbar-container d-flex align-items-center p-3'>
+      <div className='logo-container'>
+        <h1 className='page-logo'>movieDB</h1>
+      </div>
+      <div className='ms-auto d-flex align-items-center'>
+        <ul className='order-1 d-flex align-items-center p-0 mb-0 ms-3 nav-items-list'>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/'>
+              Popular
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/top-rated'>
+              Top Rated
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/upcoming'>
+              Upcoming
+            </Link>
+          </li>
+        </ul>
+        {renderSearchBar()}
+      </div>
     </nav>
   )
 }
 
-export default Navbar
+export default withRouter(NavBar)
